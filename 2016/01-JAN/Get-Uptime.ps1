f420e4cbc4931f61f2717f72fab8b28c99347c34
P0oki <kanemetal@gmail.com>
2016-02-15 23:51:10 -0900

    2016-January-Entry

---
+++ 2016/01-JAN/Get-Uptime.ps1
@@ -0,0 +1,28 @@
+﻿#2016-January Scripting Games Puzzle
+
+Function Get-Uptime
+    {
+    [CmdletBinding()]
+
+    Param
+        (
+        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [String[]] $ComputerName = $env:COMPUTERNAME
+        )
+
+    Begin
+        {
+        if (Test-Connection -ComputerName $ComputerName -Count 1 -Quiet)
+            {
+
+            }
+        }
+
+    Process
+        {
+        }
+
+    End
+        {
+        }
+    }
